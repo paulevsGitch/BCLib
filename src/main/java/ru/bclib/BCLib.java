6a5584deae1635d6c44d98e88ce150117fbd05f7
paulevsGitch <paulevs@yandex.ru>
2021-05-22 23:38:07 +0300

    Color utils, logger, math helper

--- src/main/java/ru/bclib/BCLib.java
+++ src/main/java/ru/bclib/BCLib.java
@@ -1,9 +1,11 @@
 package ru.bclib;
 
 import net.fabricmc.api.ModInitializer;
+import ru.bclib.util.Logger;
 
 public class BCLib implements ModInitializer {
 	public static final String MOD_ID = "bclib";
+	public static final Logger LOGGER = new Logger(MOD_ID);
 	
 	@Override
 	public void onInitialize() {}
