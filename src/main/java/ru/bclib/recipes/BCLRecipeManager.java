834389712b8c9966a60ed9fd392bd1535b6b9f78
paulevsGitch <paulevs@yandex.ru>
2021-05-26 16:26:48 +0300

    Accessors, recipe builders

---
+++ src/main/java/ru/bclib/recipes/BCLRecipeManager.java
@@ -0,0 +1,93 @@
+package ru.bclib.recipes;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.google.common.collect.Maps;
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+
+public class BCLRecipeManager {
+	private static final Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> RECIPES = Maps.newHashMap();
+
+	public static void addRecipe(RecipeType<?> type, Recipe<?> recipe) {
+		Map<ResourceLocation, Recipe<?>> list = RECIPES.get(type);
+		if (list == null) {
+			list = Maps.newHashMap();
+			RECIPES.put(type, list);
+		}
+		list.put(recipe.getId(), recipe);
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T extends Recipe<?>> T getRecipe(RecipeType<T> type, ResourceLocation id) {
+		if (RECIPES.containsKey(type)) {
+			return (T) RECIPES.get(type).get(id);
+		}
+		return null;
+	}
+
+	public static Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> getMap(Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> recipes) {
+		Map<RecipeType<?>, Map<ResourceLocation, Recipe<?>>> result = Maps.newHashMap();
+
+		for (RecipeType<?> type : recipes.keySet()) {
+			Map<ResourceLocation, Recipe<?>> typeList = Maps.newHashMap();
+			typeList.putAll(recipes.get(type));
+			result.put(type, typeList);
+		}
+
+		for (RecipeType<?> type : RECIPES.keySet()) {
+			Map<ResourceLocation, Recipe<?>> list = RECIPES.get(type);
+			if (list != null) {
+				Map<ResourceLocation, Recipe<?>> typeList = result.get(type);
+				if (typeList == null) {
+					typeList = Maps.newHashMap();
+					result.put(type, typeList);
+				}
+				for (Entry<ResourceLocation, Recipe<?>> entry : list.entrySet()) {
+					ResourceLocation id = entry.getKey();
+					if (!typeList.containsKey(id))
+						typeList.put(id, entry.getValue());
+				}
+			}
+		}
+
+		return result;
+	}
+
+	public static <S extends RecipeSerializer<T>, T extends Recipe<?>> S registerSerializer(String modID, String id, S serializer) {
+		return Registry.register(Registry.RECIPE_SERIALIZER, modID + ":" + id, serializer);
+	}
+
+	public static <T extends Recipe<?>> RecipeType<T> registerType(String modID, String type) {
+		ResourceLocation recipeTypeId = new ResourceLocation(modID, type);
+		return Registry.register(Registry.RECIPE_TYPE, recipeTypeId, new RecipeType<T>() {
+			public String toString() {
+				return type;
+			}
+	    });
+	}
+	
+	public static boolean exists(ItemLike item) {
+		if (item instanceof Block) {
+			return Registry.BLOCK.getKey((Block) item) != Registry.BLOCK.getDefaultKey();
+		} else {
+			return Registry.ITEM.getKey(item.asItem()) != Registry.ITEM.getDefaultKey();
+		}
+	}
+	
+	public static boolean exists(ItemLike... items) {
+		for (ItemLike item : items) {
+			if (!exists(item)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
---
+++ src/main/java/ru/bclib/recipes/FurnaceRecipe.java
@@ -0,0 +1,96 @@
+package ru.bclib.recipes;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.BlastingRecipe;
+import net.minecraft.world.item.crafting.CampfireCookingRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.SmeltingRecipe;
+import net.minecraft.world.item.crafting.SmokingRecipe;
+import net.minecraft.world.level.ItemLike;
+import ru.bclib.BCLib;
+
+public class FurnaceRecipe {
+	private static final FurnaceRecipe INSTANCE = new FurnaceRecipe();
+	
+	private ResourceLocation id;
+	private ItemLike input;
+	private ItemLike output;
+	private boolean exist;
+	private String group;
+	private int count;
+	private int time;
+	private float xp;
+	
+	private FurnaceRecipe() {}
+	
+	public static FurnaceRecipe make(String modID, String name, ItemLike input, ItemLike output) {
+		INSTANCE.id = new ResourceLocation(modID, name);
+		INSTANCE.group = "";
+		INSTANCE.input = input;
+		INSTANCE.output = output;
+		INSTANCE.count = 1;
+		INSTANCE.time = 200;
+		INSTANCE.xp = 0;
+		INSTANCE.exist = BCLRecipeManager.exists(output) && BCLRecipeManager.exists(input);
+		return INSTANCE;
+	}
+	
+	public FurnaceRecipe setGroup(String group) {
+		this.group = group;
+		return this;
+	}
+	
+	public FurnaceRecipe setOutputCount(int count) {
+		this.count = count;
+		return this;
+	}
+	
+	public FurnaceRecipe setXP(float xp) {
+		this.xp = xp;
+		return this;
+	}
+	
+	public FurnaceRecipe setCookTime(int time) {
+		this.time = time;
+		return this;
+	}
+	
+	public void build() {
+		build(false, false, false);
+	}
+	
+	public void buildWithBlasting() {
+		build(true, false, false);
+	}
+	
+	public void buildFoodlike() {
+		build(false, true, true);
+	}
+	
+	public void build(boolean blasting, boolean campfire, boolean smoker) {
+		if (exist) {
+			SmeltingRecipe recipe = new SmeltingRecipe(id, group, Ingredient.of(input), new ItemStack(output, count), xp, time);
+			BCLRecipeManager.addRecipe(RecipeType.SMELTING, recipe);
+			
+			if (blasting) {
+				BlastingRecipe recipe2 = new BlastingRecipe(id, group, Ingredient.of(input), new ItemStack(output, count), xp, time / 2);
+				BCLRecipeManager.addRecipe(RecipeType.BLASTING, recipe2);
+			}
+			
+			if (campfire) {
+				CampfireCookingRecipe recipe2 = new CampfireCookingRecipe(id, group, Ingredient.of(input), new ItemStack(output, count), xp, time * 3);
+				BCLRecipeManager.addRecipe(RecipeType.CAMPFIRE_COOKING, recipe2);
+			}
+			
+			if (smoker) {
+				SmokingRecipe recipe2 = new SmokingRecipe(id, group, Ingredient.of(input), new ItemStack(output, count), xp, time / 2);
+				BCLRecipeManager.addRecipe(RecipeType.SMOKING, recipe2);
+			}
+		}
+		else {
+			BCLib.LOGGER.debug("Furnace recipe {} couldn't be added", id);
+		}
+	}
+}
