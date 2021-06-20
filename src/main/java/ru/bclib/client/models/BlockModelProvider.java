26860898353c51cc218b0423070ada00174d4ddf
paulevsGitch <paulevs@yandex.ru>
2021-06-20 17:40:58 +0300

    Client side render fix

--- src/main/java/ru/bclib/client/models/BlockModelProvider.java
+++ src/main/java/ru/bclib/client/models/BlockModelProvider.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 import org.jetbrains.annotations.Nullable;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.client.renderer.block.model.BlockModel;
 import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.resources.ResourceLocation;
@@ -14,17 +16,20 @@ import net.minecraft.world.level.block.state.BlockState;
 import ru.bclib.BCLib;
 
 public interface BlockModelProvider extends ItemModelProvider {
+	@Environment(EnvType.CLIENT)
 	default @Nullable BlockModel getBlockModel(ResourceLocation resourceLocation, BlockState blockState) {
 		Optional<String> pattern = PatternsHelper.createBlockSimple(resourceLocation);
 		return ModelsHelper.fromPattern(pattern);
 	}
 
+	@Environment(EnvType.CLIENT)
 	default UnbakedModel getModelVariant(ResourceLocation stateId, BlockState blockState, Map<ResourceLocation, UnbakedModel> modelCache) {
 		ResourceLocation modelId = new ResourceLocation(stateId.getNamespace(), "block/" + stateId.getPath());
 		registerBlockModel(stateId, modelId, blockState, modelCache);
 		return ModelsHelper.createBlockSimple(modelId);
 	}
 
+	@Environment(EnvType.CLIENT)
 	default void registerBlockModel(ResourceLocation stateId, ResourceLocation modelId, BlockState blockState, Map<ResourceLocation, UnbakedModel> modelCache) {
 		if (!modelCache.containsKey(modelId)) {
 			BlockModel model = getBlockModel(stateId, blockState);
