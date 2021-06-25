307486a32ccf865f47521162589aa3730f01ec81
Frank Bauer <developer@ambertation.de>
2021-06-25 11:34:32 +0200

    Changes to TagLoader

--- src/main/java/ru/bclib/mixin/common/TagLoaderMixin.java
+++ src/main/java/ru/bclib/mixin/common/TagLoaderMixin.java
@@ -1,26 +1,27 @@
 package ru.bclib.mixin.common;
 
 import java.util.Map;
-import java.util.concurrent.Executor;
-import java.util.function.Supplier;
 
+import net.minecraft.server.packs.resources.ResourceManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Inject;
 
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.Tag;
 import net.minecraft.tags.TagLoader;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import ru.bclib.util.TagHelper;
 
 @Mixin(TagLoader.class)
 public class TagLoaderMixin {
 	@Shadow
-	private String name;
+	private String directory;
 
-	@ModifyArg(method = "prepare", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/CompletableFuture;supplyAsync(Ljava/util/function/Supplier;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
-	public Supplier<Map<ResourceLocation, Tag.Builder>> be_modifyTags(Supplier<Map<ResourceLocation, Tag.Builder>> supplier, Executor executor) {
-		return () -> TagHelper.apply(name, supplier.get());
+	//@ModifyArg(method = "prepare", at = @At(value = "INVOKE", target = "Ljava/util/concurrent/CompletableFuture;supplyAsync(Ljava/util/function/Supplier;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"))
+	@Inject(method="load", at=@At(value="RETURN"))
+	public void be_modifyTags(ResourceManager resourceManager, CallbackInfoReturnable<Map<ResourceLocation, Tag.Builder>> cir) {
+		TagHelper.apply(directory, cir.getReturnValue());
 	}
 }
