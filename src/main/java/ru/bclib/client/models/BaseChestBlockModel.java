ad40784871b62443c98caff880f5bc58e9e4a3ef
Frank Bauer <developer@ambertation.de>
2021-06-28 17:38:11 +0200

    `texOffs` before `addBox`

--- src/main/java/ru/bclib/client/models/BaseChestBlockModel.java
+++ src/main/java/ru/bclib/client/models/BaseChestBlockModel.java
@@ -20,56 +20,56 @@ public class BaseChestBlockModel {
         PartDefinition modelPartData = modelData.getRoot();
         CubeDeformation deformation_partC = new CubeDeformation(0.0f);
         modelPartData.addOrReplaceChild("partC", CubeListBuilder.create()
-                        .addBox(1.0f, 0.0f, 1.0f, 14.0f, 9.0f, 14.0f, deformation_partC)
-                        .texOffs(0, 19),
+                        .texOffs(0, 19)
+                        .addBox(1.0f, 0.0f, 1.0f, 14.0f, 9.0f, 14.0f, deformation_partC),
                 PartPose.ZERO);
 
         CubeDeformation deformation_partA = new CubeDeformation(0.0f);
         modelPartData.addOrReplaceChild("partA", CubeListBuilder.create()
-                        .addBox(1.0f, 0.0f, 0.0f, 14.0f, 5.0f, 14.0f, deformation_partA)
-                        .texOffs(0, 0),
+                        .texOffs(0, 0)
+                        .addBox(1.0f, 0.0f, 0.0f, 14.0f, 5.0f, 14.0f, deformation_partA),
                 PartPose.offset(0.0f, 9.0f, 1.0f));
 
         CubeDeformation deformation_partB = new CubeDeformation(0.0f);
         modelPartData.addOrReplaceChild("partB", CubeListBuilder.create()
-                        .addBox(7.0f, -1.0f, 15.0f, 2.0f, 4.0f, 1.0f, deformation_partB)
-                        .texOffs(0, 0),
+                        .texOffs(0, 0)
+                        .addBox(7.0f, -1.0f, 15.0f, 2.0f, 4.0f, 1.0f, deformation_partB),
                 PartPose.offset(0.0f, 8.0f, 0.0f));
 
         CubeDeformation deformation_partRightC = new CubeDeformation(0.0f);
         modelPartData.addOrReplaceChild("partRightC", CubeListBuilder.create()
-                        .addBox(1.0f, 0.0f, 1.0f, 15.0f, 9.0f, 14.0f, deformation_partRightC)
-                        .texOffs(0, 19),
+                        .texOffs(0, 19)
+                        .addBox(1.0f, 0.0f, 1.0f, 15.0f, 9.0f, 14.0f, deformation_partRightC),
                 PartPose.ZERO);
 
         CubeDeformation deformation_partRightA = new CubeDeformation(0.0f);
         modelPartData.addOrReplaceChild("partRightA", CubeListBuilder.create()
-                        .addBox(1.0f, 0.0f, 0.0f, 15.0f, 5.0f, 14.0f, deformation_partRightA)
-                        .texOffs(0, 0),
+                        .texOffs(0, 0)
+                        .addBox(1.0f, 0.0f, 0.0f, 15.0f, 5.0f, 14.0f, deformation_partRightA),
                 PartPose.offset(0.0f, 9.0f, 1.0f));
 
         CubeDeformation deformation_partRightB = new CubeDeformation(0.0f);
         PartDefinition partRightB = modelPartData.addOrReplaceChild("partRightB", CubeListBuilder.create()
-                        .addBox(15.0f, -1.0f, 15.0f, 1.0f, 4.0f, 1.0f, deformation_partRightB)
-                        .texOffs(0, 0),
+                        .texOffs(0, 0)
+                        .addBox(15.0f, -1.0f, 15.0f, 1.0f, 4.0f, 1.0f, deformation_partRightB),
                 PartPose.offset(0.0f, 8.0f, 0.0f));
 
         CubeDeformation deformation_partLeftC = new CubeDeformation(0.0f);
         modelPartData.addOrReplaceChild("partLeftC", CubeListBuilder.create()
-                        .addBox(0.0f, 0.0f, 1.0f, 15.0f, 9.0f, 14.0f, deformation_partLeftC)
-                        .texOffs(0, 19),
+                        .texOffs(0, 19)
+                        .addBox(0.0f, 0.0f, 1.0f, 15.0f, 9.0f, 14.0f, deformation_partLeftC),
                 PartPose.ZERO);
 
         CubeDeformation deformation_partLeftA = new CubeDeformation(0.0f);
         modelPartData.addOrReplaceChild("partLeftA", CubeListBuilder.create()
-                        .addBox(0.0f, 0.0f, 0.0f, 15.0f, 5.0f, 14.0f, deformation_partLeftA)
-                        .texOffs(0, 0),
+                        .texOffs(0, 0)
+                        .addBox(0.0f, 0.0f, 0.0f, 15.0f, 5.0f, 14.0f, deformation_partLeftA),
                 PartPose.offset(0.0f, 9.0f, 1.0f));
 
         CubeDeformation deformation_partLeftB = new CubeDeformation(0.0f);
         modelPartData.addOrReplaceChild("partLeftB", CubeListBuilder.create()
-                        .addBox(0.0f, -1.0f, 15.0f, 1.0f, 4.0f, 1.0f, deformation_partLeftB)
-                        .texOffs(0, 0),
+                        .texOffs(0, 0)
+                        .addBox(0.0f, -1.0f, 15.0f, 1.0f, 4.0f, 1.0f, deformation_partLeftB),
                 PartPose.offset(0.0f, 8.0f, 0.0f));
 
         return LayerDefinition.create(modelData, 64, 64);
