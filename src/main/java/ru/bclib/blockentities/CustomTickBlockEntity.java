447a19937969e50219102a074c204d459ebcf20a
Frank Bauer <developer@ambertation.de>
2021-06-29 08:09:22 +0200

    Revert "re-enable custom tick functions for furnaces"
    This reverts commit b54b14782e9bd0fdfe12cacbdc6529072551732c.

--- src/main/java/ru/bclib/blockentities/CustomTickBlockEntity.java
+++
@@ -1,9 +0,0 @@
-package ru.bclib.blockentities;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-
-public interface CustomTickBlockEntity {
-    void customTick(Level level, BlockPos pos, BlockState state);
-}
