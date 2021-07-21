f981527200e1bb9cdf09fdec60b911b41d184e12
paulevsGitch <paulevs@yandex.ru>
2021-07-21 17:31:24 +0300

    Feature randomization (fix MC bug)

--- src/main/java/ru/bclib/world/features/BCLFeature.java
+++ src/main/java/ru/bclib/world/features/BCLFeature.java
@@ -39,23 +39,25 @@ public class BCLFeature {
 	}
 	
 	public static BCLFeature makeVegetationFeature(ResourceLocation id, Feature<NoneFeatureConfiguration> feature, int density) {
-		ConfiguredFeature<?, ?> configured = feature.configured(FeatureConfiguration.NONE)
-													.decorated(BCLDecorators.HEIGHTMAP_SQUARE)
-													.countRandom(density);
+		ConfiguredFeature<?, ?> configured = feature
+			.configured(FeatureConfiguration.NONE)
+			.decorated(BCLDecorators.HEIGHTMAP_SQUARE)
+			.countRandom(density);
 		return new BCLFeature(id, feature, GenerationStep.Decoration.VEGETAL_DECORATION, configured);
 	}
 	
 	public static BCLFeature makeRawGenFeature(ResourceLocation id, Feature<NoneFeatureConfiguration> feature, int chance) {
-		ConfiguredFeature<?, ?> configured = feature.configured(FeatureConfiguration.NONE)
-													.decorated(FeatureDecorator.CHANCE.configured(new ChanceDecoratorConfiguration(
-														chance)));
+		ConfiguredFeature<?, ?> configured = feature
+			.configured(FeatureConfiguration.NONE)
+			.decorated(FeatureDecorator.CHANCE.configured(new ChanceDecoratorConfiguration(chance)));
 		return new BCLFeature(id, feature, GenerationStep.Decoration.RAW_GENERATION, configured);
 	}
 	
+	@Deprecated
 	public static BCLFeature makeLakeFeature(ResourceLocation id, Feature<NoneFeatureConfiguration> feature, int chance) {
-		ConfiguredFeature<?, ?> configured = feature.configured(FeatureConfiguration.NONE)
-													.decorated(FeatureDecorator.LAVA_LAKE.configured(new ChanceDecoratorConfiguration(
-														chance)));
+		ConfiguredFeature<?, ?> configured = feature
+			.configured(FeatureConfiguration.NONE)
+			.decorated(FeatureDecorator.LAVA_LAKE.configured(new ChanceDecoratorConfiguration(chance)));
 		return new BCLFeature(id, feature, GenerationStep.Decoration.LAKES, configured);
 	}
 	
@@ -66,13 +68,14 @@ public class BCLFeature {
 			veinSize
 		);
 		OreConfiguration config = new OreConfiguration(ANY_TERRAIN, blockOre.defaultBlockState(), 33);
-		ConfiguredFeature<?, ?> oreFeature = Feature.ORE.configured(featureConfig)
-														.rangeUniform(
-															VerticalAnchor.absolute(minY),
-															VerticalAnchor.absolute(maxY)
-														)
-														.squared()
-														.count(veins);
+		ConfiguredFeature<?, ?> oreFeature = Feature.ORE
+			.configured(featureConfig)
+			.rangeUniform(
+				VerticalAnchor.absolute(minY),
+				VerticalAnchor.absolute(maxY)
+			)
+			.squared()
+			.count(veins);
 		return new BCLFeature(
 			Feature.ORE,
 			Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, id, oreFeature),
@@ -81,23 +84,22 @@ public class BCLFeature {
 	}
 	
 	public static BCLFeature makeChunkFeature(ResourceLocation id, Feature<NoneFeatureConfiguration> feature) {
-		ConfiguredFeature<?, ?> configured = feature.configured(FeatureConfiguration.NONE)
-													.decorated(FeatureDecorator.COUNT.configured(new CountConfiguration(
-														1)));
+		ConfiguredFeature<?, ?> configured = feature
+			.configured(FeatureConfiguration.NONE)
+			.decorated(FeatureDecorator.COUNT.configured(new CountConfiguration(1)));
 		return new BCLFeature(id, feature, GenerationStep.Decoration.LOCAL_MODIFICATIONS, configured);
 	}
 	
 	public static BCLFeature makeChansedFeature(ResourceLocation id, Feature<NoneFeatureConfiguration> feature, int chance) {
-		ConfiguredFeature<?, ?> configured = feature.configured(FeatureConfiguration.NONE)
-													.decorated(FeatureDecorator.CHANCE.configured(new ChanceDecoratorConfiguration(
-														chance)));
+		ConfiguredFeature<?, ?> configured = feature
+			.configured(FeatureConfiguration.NONE)
+			.decorated(FeatureDecorator.CHANCE.configured(new ChanceDecoratorConfiguration(chance)));
 		return new BCLFeature(id, feature, GenerationStep.Decoration.SURFACE_STRUCTURES, configured);
 	}
 	
 	public static BCLFeature makeCountRawFeature(ResourceLocation id, Feature<NoneFeatureConfiguration> feature, int chance) {
 		ConfiguredFeature<?, ?> configured = feature.configured(FeatureConfiguration.NONE)
-													.decorated(FeatureDecorator.COUNT.configured(new CountConfiguration(
-														chance)));
+			.decorated(FeatureDecorator.COUNT.configured(new CountConfiguration(chance)));
 		return new BCLFeature(id, feature, GenerationStep.Decoration.RAW_GENERATION, configured);
 	}
 	
