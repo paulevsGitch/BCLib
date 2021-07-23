edf9e1d004c24533c69353b69496f5473463e8b6
paulevsGitch <paulevs@yandex.ru>
2021-07-23 13:41:54 +0300

    Material init

--- src/main/java/ru/bclib/client/BCLibClient.java
+++ src/main/java/ru/bclib/client/BCLibClient.java
@@ -7,6 +7,7 @@ import net.minecraft.core.Registry;
 import ru.bclib.api.ModIntegrationAPI;
 import ru.bclib.api.PostInitAPI;
 import ru.bclib.client.render.BCLRenderLayer;
+import ru.bclib.complexmaterials.ComplexMaterial;
 import ru.bclib.interfaces.PostInitable;
 import ru.bclib.interfaces.RenderLayerProvider;
 import ru.bclib.registry.BaseBlockEntityRenders;
@@ -14,6 +15,7 @@ import ru.bclib.registry.BaseBlockEntityRenders;
 public class BCLibClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
+		ComplexMaterial.getAllMaterials().forEach(material -> material.init());
 		ModIntegrationAPI.registerAll();
 		BaseBlockEntityRenders.register();
 		PostInitAPI.postInit(true);
--- src/main/java/ru/bclib/server/BCLibServer.java
+++ src/main/java/ru/bclib/server/BCLibServer.java
@@ -4,11 +4,13 @@ import net.fabricmc.api.DedicatedServerModInitializer;
 import net.minecraft.core.Registry;
 import ru.bclib.api.ModIntegrationAPI;
 import ru.bclib.api.PostInitAPI;
+import ru.bclib.complexmaterials.ComplexMaterial;
 import ru.bclib.interfaces.PostInitable;
 
 public class BCLibServer implements DedicatedServerModInitializer {
 	@Override
 	public void onInitializeServer() {
+		ComplexMaterial.getAllMaterials().forEach(material -> material.init());
 		ModIntegrationAPI.registerAll();
 		PostInitAPI.postInit(false);
 	}
