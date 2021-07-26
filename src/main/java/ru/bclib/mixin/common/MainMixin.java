d3db623de59883ff905d236600c1adda1466f8e6
Frank Bauer <developer@ambertation.de>
2021-07-26 18:50:15 +0200

    Call DataFixer in Server Context

---
+++ src/main/java/ru/bclib/mixin/common/MainMixin.java
@@ -0,0 +1,17 @@
+package ru.bclib.mixin.common;
+
+import net.minecraft.server.Main;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import ru.bclib.api.datafixer.DataFixerAPI;
+
+@Mixin(Main.class)
+abstract public class MainMixin {
+	@ModifyArg(method="main", at=@At(value="INVOKE", target="Lnet/minecraft/server/MinecraftServer;convertFromRegionFormatIfNeeded(Lnet/minecraft/world/level/storage/LevelStorageSource$LevelStorageAccess;)V"))
+	private static LevelStorageSource.LevelStorageAccess bclib_callServerFix(LevelStorageSource.LevelStorageAccess session){
+		DataFixerAPI.fixData(session, false, (didFix)->{});
+		return session;
+	}
+}
