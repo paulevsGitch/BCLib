7632c4e498e4b5af97ace04f396d08b4f4f3dc44
Frank Bauer <developer@ambertation.de>
2021-07-28 23:52:18 +0200

    Send first test-message

--- src/main/java/ru/bclib/api/dataexchange/TestHandler.java
+++ src/main/java/ru/bclib/api/dataexchange/TestHandler.java
@@ -8,6 +8,8 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import ru.bclib.BCLib;
 
+import java.nio.charset.StandardCharsets;
+
 public class TestHandler extends DataHandler{
 	public static DataHandlerDescriptor DESCRIPTOR = new DataHandlerDescriptor(new ResourceLocation(BCLib.MOD_ID, "__test"), TestHandler::new, true);
 	
@@ -17,7 +19,9 @@ public class TestHandler extends DataHandler{
 	
 	@Override
 	protected void deserializeFromIncomingData(FriendlyByteBuf buf, PacketSender responseSender, boolean fromClient) {
-		BCLib.LOGGER.info("PROCESSING INCOMING TEST-DATA fromClient="+fromClient);
+		int length = buf.readInt();
+		CharSequence text = buf.readCharSequence(length, StandardCharsets.UTF_8);
+		BCLib.LOGGER.info("PROCESSING INCOMING TEST-DATA fromClient="+fromClient+": "+text);
 	}
 	
 	@Override
@@ -28,6 +32,9 @@ public class TestHandler extends DataHandler{
 	
 	@Override
 	protected void serializeData(FriendlyByteBuf buf) {
+		CharSequence text = "Welcome from BCLib";
+		buf.writeInt(text.length());
+		buf.writeCharSequence(text, StandardCharsets.UTF_8);
 		BCLib.LOGGER.info("BUILDING OUTGOING TEST-DATA ON SERVER");
 	}
 }
