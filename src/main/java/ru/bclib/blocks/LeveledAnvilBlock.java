2674940c91cc68eaa2fbc397887c48dd967e43eb
Frank <developer@ambertation.de>
2021-10-18 12:26:35 +0200

    Added AnvilReceipts from BE

---
+++ src/main/java/ru/bclib/blocks/LeveledAnvilBlock.java
@@ -0,0 +1,16 @@
+package ru.bclib.blocks;
+
+import net.minecraft.world.level.material.MaterialColor;
+
+public class LeveledAnvilBlock extends BaseAnvilBlock{
+        protected final int level;
+
+        public LeveledAnvilBlock(MaterialColor color, int level) {
+            super(color);
+            this.level = level;
+        }
+
+        public int getCraftingLevel() {
+            return level;
+        }
+}
