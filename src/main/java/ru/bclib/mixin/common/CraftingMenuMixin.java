5ad8752e00c515fcb3f8a0462fe40ceba83113c6
paulevsGitch <paulevs@yandex.ru>
2021-11-13 11:33:04 +0300

    Crafting menu fix

---
+++ src/main/java/ru/bclib/mixin/common/CraftingMenuMixin.java
@@ -0,0 +1,31 @@
+package ru.bclib.mixin.common;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.inventory.CraftingMenu;
+import net.minecraft.world.level.block.CraftingTableBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import ru.bclib.api.TagAPI;
+
+@Mixin(CraftingMenu.class)
+public abstract class CraftingMenuMixin {
+	@Final
+	@Shadow
+	private ContainerLevelAccess access;
+	
+	@Inject(method = "stillValid", at = @At("HEAD"), cancellable = true)
+	private void bclib_stillValid(Player player, CallbackInfoReturnable<Boolean> info) {
+		if (access.evaluate((world, pos) -> {
+			BlockState state = world.getBlockState(pos);
+			return state.getBlock() instanceof CraftingTableBlock || state.is(TagAPI.BLOCK_WORKBENCHES);
+		}, true)) {
+			info.setReturnValue(true);
+		}
+	}
+}
