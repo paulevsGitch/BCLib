60251a7b79a65f9d90cc92355e559064089109e6
paulevsGitch <paulevs@yandex.ru>
2021-12-04 15:32:05 +0300

    Cleanup

--- src/main/java/ru/bclib/world/biomes/BCLBiome.java
+++ src/main/java/ru/bclib/world/biomes/BCLBiome.java
@@ -18,7 +18,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.function.Consumer;
-import java.util.function.Supplier;
 
 public class BCLBiome {
 	private final List<ConfiguredStructureFeature> structures = Lists.newArrayList();
