77801986b5c83333e10f2f02a01d2b23f70218fa
Frank <developer@ambertation.de>
2021-12-15 11:34:29 +0100

    Change Interface for BE

--- src/main/java/ru/bclib/interfaces/SurfaceMaterialProvider.java
+++ src/main/java/ru/bclib/interfaces/SurfaceMaterialProvider.java
@@ -1,9 +1,13 @@
 package ru.bclib.interfaces;
 
 import net.minecraft.world.level.block.state.BlockState;
+import ru.bclib.api.surface.SurfaceRuleBuilder;
 
 public interface SurfaceMaterialProvider {
     BlockState getTopMaterial();
     BlockState getUnderMaterial();
     BlockState getAltTopMaterial();
+
+    boolean generateFloorRule();
+    SurfaceRuleBuilder surface();
 }
