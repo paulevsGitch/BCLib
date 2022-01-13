ec619727248691f1031b416dc59a984172a519ef
paulevsGitch <paulevs@yandex.ru>
2022-01-13 18:51:59 +0300

    Enhanced Enchantment Menu and Table mixins (#72)

--- src/main/java/ru/bclib/mixin/client/EnchantingTableBlockMixin.java
+++ src/main/java/ru/bclib/mixin/client/EnchantingTableBlockMixin.java
@@ -1,18 +1,13 @@
 package ru.bclib.mixin.client;
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.EnchantmentTableBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import ru.bclib.api.TagAPI;
-
-import java.util.Random;
 
 @Mixin(EnchantmentTableBlock.class)
 public abstract class EnchantingTableBlockMixin extends Block {
@@ -20,34 +15,11 @@ public abstract class EnchantingTableBlockMixin extends Block {
 		super(settings);
 	}
 	
-	@Inject(method = "animateTick", at = @At(value = "TAIL"))
-	private void be_onRandomDisplayTick(BlockState state, Level world, BlockPos pos, Random random, CallbackInfo info) {
-		for (int px = -2; px <= 2; ++px) {
-			for (int pz = -2; pz <= 2; ++pz) {
-				if (px > -2 && px < 2 && pz == -1) {
-					pz = 2;
-				}
-				if (random.nextInt(16) == 0) {
-					for (int py = 0; py <= 1; ++py) {
-						BlockPos blockPos = pos.offset(px, py, pz);
-						if (world.getBlockState(blockPos).is(TagAPI.BLOCK_BOOKSHELVES)) {
-							if (!world.isEmptyBlock(pos.offset(px / 2, 0, pz / 2))) {
-								break;
-							}
-							world.addParticle(
-								ParticleTypes.ENCHANT,
-								pos.getX() + 0.5,
-								pos.getY() + 2.0,
-								pos.getZ() + 0.5,
-								px + random.nextFloat() - 0.5,
-								py - random.nextFloat() - 1.0,
-								pz + random.nextFloat() - 0.5
-							);
-						}
-					}
-				}
-			}
-		}
-		
+	@Redirect(method = "animateTick", at = @At(
+		value = "INVOKE",
+		target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z")//,
+	)
+	private boolean bclib_isBookshelf(BlockState state, Block block) {
+		return block == Blocks.BOOKSHELF ? state.is(TagAPI.BLOCK_BOOKSHELVES) : state.is(block);
 	}
 }
