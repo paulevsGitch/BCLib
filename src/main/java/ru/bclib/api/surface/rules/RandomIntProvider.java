44517e07490d702219d30f5e6438eecd99cd4421
Frank <developer@ambertation.de>
2022-01-20 03:36:39 +0100

    More Codecs

--- src/main/java/ru/bclib/api/surface/rules/RandomIntProvider.java
+++ src/main/java/ru/bclib/api/surface/rules/RandomIntProvider.java
@@ -1,12 +1,19 @@
 package ru.bclib.api.surface.rules;
 
+import com.mojang.serialization.Codec;
 import ru.bclib.interfaces.NumericProvider;
 import ru.bclib.mixin.common.SurfaceRulesContextAccessor;
 import ru.bclib.util.MHelper;
 
 public record RandomIntProvider(int range) implements NumericProvider {
+	public static final Codec<RandomIntProvider> CODEC = Codec.INT.fieldOf("nethrangeer_noise").xmap(RandomIntProvider::new, obj -> obj.range).codec();
 	@Override
 	public int getNumber(SurfaceRulesContextAccessor context) {
 		return MHelper.RANDOM.nextInt(range);
+	}
+
+	@Override
+	public Codec<? extends NumericProvider> pcodec() {
+		return CODEC;
 	}
 }
--- src/main/java/ru/bclib/interfaces/NumericProvider.java
+++ src/main/java/ru/bclib/interfaces/NumericProvider.java
@@ -1,7 +1,14 @@
 package ru.bclib.interfaces;
 
+import com.mojang.serialization.Codec;
+import net.minecraft.core.Registry;
+import net.minecraft.world.level.levelgen.SurfaceRules;
 import ru.bclib.mixin.common.SurfaceRulesContextAccessor;
+
+import java.util.function.Function;
 
 public interface NumericProvider {
 	int getNumber(SurfaceRulesContextAccessor context);
+
+	Codec<? extends NumericProvider> pcodec();
 }
